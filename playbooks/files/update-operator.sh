28ea86642643e7717ff2c1d887826a3b6bd968c1
Tristan Cacqueray <tdecacqu@redhat.com>
2020-03-20 16:13:19 +0000

    Adapt the integration playbook to be usable locally
    This change adds some utility files to help with
    running the zuul job locally.
    
    Change-Id: I427ca493e456f4d80350921212d3bbcccbf1c6be

---
+++ playbooks/files/update-operator.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -e
+# Update the operator image
+echo "Remove previous operator"
+kubectl delete -f deploy/operator.yaml || :
+
+BUILDAH_OPTS=${BUILDAH_OPTS:-}
+if test -d /var/lib/silverkube/storage; then
+    BUILDAH_OPTS="${BUILDAH_OPTS} --root /var/lib/silverkube/storage --storage-driver vfs"
+fi
+
+echo "Update local image"
+CTX=$(sudo buildah from  ${BUILDAH_OPTS} docker.io/zuul/zuul-operator:latest)
+MNT=$(sudo buildah mount ${BUILDAH_OPTS} $CTX)
+
+sudo rsync -avi --delete roles/ ${MNT}/opt/ansible/roles/
+sudo rsync -avi --delete conf/ ${MNT}/opt/ansible/conf/
+
+sudo buildah commit ${BUILDAH_OPTS} --rm ${CTX} docker.io/zuul/zuul-operator:latest
+
+kubectl apply -f deploy/operator.yaml
