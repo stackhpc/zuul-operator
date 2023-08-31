41f612df6a5e9efc55aebb6b28ab6868ab007393
Will <iq20064@bristol.ac.uk>
2023-08-31 11:36:39 +0100

    Deployment configuration and setup scripts

--- install.sh
+++
@@ -1,6 +0,0 @@
-kubectl apply -f deploy/crds/zuul-ci_v1alpha2_zuul_crd.yaml
-kubectl apply -f deploy/rbac-admin.yaml
-kubectl apply -f deploy/operator.yaml
-kubectl create secret generic zuul-nodepool-config --from-file=custom-deployment-files/nodepool.yaml
-kubectl create secret generic zuul-tenant-config --from-file=custom-deployment-files/main.yaml
-kubectl create secret generic executor-secrets --from-file=keys/id_rsa
