e798842cce66e499f8b43023749247bc184fdd3e
Will <iq20064@bristol.ac.uk>
2023-08-31 11:39:50 +0100

    Fixed patch.sh

--- stackhpc/patch.sh
+++ stackhpc/patch.sh
@@ -1,1 +1,2 @@
-kubectl apply -f "patch-files/*.yaml"
+kubectl patch deployment zuul-web --patch-file=patch-files/key-patchfile.yaml
+kubectl patch service zuul-web  --patch-file=patch-files/web-service-patchfile.yaml
